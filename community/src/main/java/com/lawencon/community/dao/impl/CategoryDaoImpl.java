93586a9bd293c4f6e5402cd0bf2dfbc64e72e0ae
ivanigk13 <ivangrashinton@gmail.com>
2022-03-23 09:37:58 +0700

    add category

---
+++ community/src/main/java/com/lawencon/community/dao/impl/CategoryDaoImpl.java
@@ -0,0 +1,8 @@
+package com.lawencon.community.dao.impl;
+
+import com.lawencon.base.BaseDaoImpl;
+import com.lawencon.community.model.Category;
+
+public class CategoryDaoImpl extends BaseDaoImpl<Category>{
+
+}
---
+++ community/src/main/java/com/lawencon/community/model/Category.java
@@ -0,0 +1,32 @@
+package com.lawencon.community.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.lawencon.base.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(uniqueConstraints = {
+	@UniqueConstraint(
+		name = "category_bk",
+		columnNames = {"category_code"}
+	),
+	@UniqueConstraint(
+		name = "category_ck",
+		columnNames = {"category_code", "category_name"}
+	)
+})
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Category extends BaseEntity{
+	
+	private static final long serialVersionUID = -5449659961400155560L;
+	private String categoryCode;
+	private String categoryName;
+
+}
