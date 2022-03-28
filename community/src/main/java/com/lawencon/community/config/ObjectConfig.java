40bedb076ddabc3cc4426cbf7759be6b7ced284d
ivanigk13 <ivangrashinton@gmail.com>
2022-03-28 11:51:28 +0700

    un comment liquibase

--- community/src/main/java/com/lawencon/community/config/ObjectConfig.java
+++ community/src/main/java/com/lawencon/community/config/ObjectConfig.java
@@ -1,9 +1,9 @@
 package com.lawencon.community.config;
 
-//import javax.sql.DataSource;
+import javax.sql.DataSource;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.DependsOn;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,27 +12,27 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.PathItem.HttpMethod;
-//import liquibase.integration.spring.SpringLiquibase;
+import liquibase.integration.spring.SpringLiquibase;
 
 @Configuration
 public class ObjectConfig {
 
-//	@Bean("inittable")
-//	public SpringLiquibase liquibaseTable(@Autowired DataSource dataSource) {
-//		SpringLiquibase liquibase = new SpringLiquibase();
-//		liquibase.setChangeLog("/db/migration/script/init_table.sql");
-//		liquibase.setDataSource(dataSource);
-//		return liquibase;
-//	}
-//	
-//	@Bean
-//	@DependsOn("inittable")
-//	public SpringLiquibase liquibaseData(@Autowired DataSource dataSource) {
-//		SpringLiquibase liquibase = new SpringLiquibase();
-//		liquibase.setChangeLog("/db/migration/script/init_data.sql");
-//		liquibase.setDataSource(dataSource);
-//		return liquibase;
-//	}
+	@Bean("inittable")
+	public SpringLiquibase liquibaseTable(@Autowired DataSource dataSource) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:db/migration/script/init_table.sql");
+		liquibase.setDataSource(dataSource);
+		return liquibase;
+	}
+	
+	@Bean
+	@DependsOn("inittable")
+	public SpringLiquibase liquibaseData(@Autowired DataSource dataSource) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:db/migration/script/init_data.sql");
+		liquibase.setDataSource(dataSource);
+		return liquibase;
+	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
