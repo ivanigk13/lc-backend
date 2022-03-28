ef8d4e4462ec9692715af5daad30fc62ae3b54c5
henatriana <henat29@gmail.com>
2022-03-28 17:21:43 +0700

    Edit RequestBody IndustryController

--- community/src/main/java/com/lawencon/community/controller/IndustryController.java
+++ community/src/main/java/com/lawencon/community/controller/IndustryController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawencon.community.dto.industry.DeleteIndustryDtoRes;
@@ -33,13 +33,13 @@ public class IndustryController {
 	private final IndustryService industryService;
 	
 	@PostMapping
-	public ResponseEntity<InsertIndustryDtoRes> insert(@RequestPart @Valid InsertIndustryDtoReq data) throws Exception{
+	public ResponseEntity<InsertIndustryDtoRes> insert(@RequestBody @Valid InsertIndustryDtoReq data) throws Exception{
 		InsertIndustryDtoRes industry = industryService.insert(data);
 		return new ResponseEntity<InsertIndustryDtoRes>(industry, HttpStatus.CREATED);		
 	}
 	
 	@PutMapping
-	public ResponseEntity<UpdateIndustryDtoRes> update(@RequestPart @Valid UpdateIndustryDtoReq data) throws Exception{
+	public ResponseEntity<UpdateIndustryDtoRes> update(@RequestBody @Valid UpdateIndustryDtoReq data) throws Exception{
 		UpdateIndustryDtoRes industry = industryService.update(data);
 		return new ResponseEntity<UpdateIndustryDtoRes>(industry, HttpStatus.OK);		
 	}
