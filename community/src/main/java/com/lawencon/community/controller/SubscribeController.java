9cb291215f244a48d3d1f3c1d2cc4ab5014a7766
henatriana <henat29@gmail.com>
2022-03-30 17:51:53 +0700

    fixed social media and subscribe controller

--- community/src/main/java/com/lawencon/community/controller/SubscribeController.java
+++ community/src/main/java/com/lawencon/community/controller/SubscribeController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawencon.community.dto.subscribe.DeleteSubscribeDtoRes;
@@ -47,13 +47,13 @@ public class SubscribeController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<InsertSubscribeDtoRes> insert(@RequestPart @Valid InsertSubscribeDtoReq data) throws Exception{
+	public ResponseEntity<InsertSubscribeDtoRes> insert(@RequestBody @Valid InsertSubscribeDtoReq data) throws Exception{
 		InsertSubscribeDtoRes subscribe = subscribeService.insert(data);
 		return new ResponseEntity<InsertSubscribeDtoRes>(subscribe, HttpStatus.CREATED);		
 	}
 	
 	@PutMapping
-	public ResponseEntity<UpdateSubscribeDtoRes> update(@RequestPart @Valid UpdateSubscribeDtoReq data) throws Exception{
+	public ResponseEntity<UpdateSubscribeDtoRes> update(@RequestBody @Valid UpdateSubscribeDtoReq data) throws Exception{
 		UpdateSubscribeDtoRes subscribe = subscribeService.update(data);
 		return new ResponseEntity<UpdateSubscribeDtoRes>(subscribe, HttpStatus.OK);		
 	}
