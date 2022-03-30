a620afcc4963055ceb6d0437bd314b4987a05914
henatriana <henat29@gmail.com>
2022-03-30 17:53:47 +0700

    fixed model order and dto subscribe

--- community/src/main/java/com/lawencon/community/dto/subscribe/UpdateSubscribeDtoReq.java
+++ community/src/main/java/com/lawencon/community/dto/subscribe/UpdateSubscribeDtoReq.java
@@ -3,7 +3,6 @@ package com.lawencon.community.dto.subscribe;
 import java.math.BigDecimal;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -14,11 +13,9 @@ public class UpdateSubscribeDtoReq {
 	private String id;
 	
 	@NotNull(message = "Duration must be filled")
-	@Size(min = 1, message = "Duration must be at least 1")
 	private Integer duration;
 	
 	@NotNull(message = "Price must be filled")
-	@Size(min = 30000, message = "price must be at least 30000")
 	private BigDecimal price;
 	
 	@NotNull(message = "Version must be filled")
