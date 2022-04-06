3947faa9c750f6c932139fe08275661b22d8e37e
ivanigk13 <ivangrashinton@gmail.com>
2022-04-06 10:48:00 +0700

    adding get last two controller in activity and article

--- community/src/main/java/com/lawencon/community/controller/ArticleController.java
+++ community/src/main/java/com/lawencon/community/controller/ArticleController.java
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.lawencon.community.dto.article.DeleteArticleDtoRes;
 import com.lawencon.community.dto.article.GetAllArticleDtoRes;
 import com.lawencon.community.dto.article.GetByIdArticleDtoRes;
+import com.lawencon.community.dto.article.GetLastTwoArticleDtoRes;
 import com.lawencon.community.dto.article.InsertArticleDtoRes;
 import com.lawencon.community.dto.article.UpdateArticleDtoRes;
 import com.lawencon.community.service.ArticleService;
@@ -33,34 +34,40 @@ public class ArticleController {
 	public ResponseEntity<GetAllArticleDtoRes> getAll(
 							@RequestParam(value = "start", required = false) Integer start,
 							@RequestParam(value = "max", required = false) Integer max) throws Exception{
-		GetAllArticleDtoRes thread = articleService.getAll(start, max);
-		return new ResponseEntity<GetAllArticleDtoRes>(thread, HttpStatus.OK);		
+		GetAllArticleDtoRes article = articleService.getAll(start, max);
+		return new ResponseEntity<GetAllArticleDtoRes>(article, HttpStatus.OK);		
 	}
 	
 	@GetMapping("{id}")
 	public ResponseEntity<GetByIdArticleDtoRes> getById(@PathVariable("id") String id) throws Exception{
-		GetByIdArticleDtoRes thread = articleService.getById(id);
-		return new ResponseEntity<GetByIdArticleDtoRes>(thread, HttpStatus.OK);		
+		GetByIdArticleDtoRes article = articleService.getById(id);
+		return new ResponseEntity<GetByIdArticleDtoRes>(article, HttpStatus.OK);		
 	}
 	
 	@PostMapping
 	public ResponseEntity<InsertArticleDtoRes> insert(
 								@RequestPart(name="data") String data, 
 								@RequestPart(required = false) MultipartFile file) throws Exception{
-		InsertArticleDtoRes thread = articleService.insert(data, file);
-		return new ResponseEntity<InsertArticleDtoRes>(thread, HttpStatus.CREATED);		
+		InsertArticleDtoRes article = articleService.insert(data, file);
+		return new ResponseEntity<InsertArticleDtoRes>(article, HttpStatus.CREATED);		
 	}
 	
 	@PutMapping
 	public ResponseEntity<UpdateArticleDtoRes> update(@RequestPart(name="data") String data, 
 													@RequestPart(required = false) MultipartFile file) throws Exception{
-		UpdateArticleDtoRes thread = articleService.update(data, file);
-		return new ResponseEntity<UpdateArticleDtoRes>(thread, HttpStatus.OK);		
+		UpdateArticleDtoRes article = articleService.update(data, file);
+		return new ResponseEntity<UpdateArticleDtoRes>(article, HttpStatus.OK);		
 	}
 	
 	@DeleteMapping("{id}")
 	public ResponseEntity<DeleteArticleDtoRes> delete(@PathVariable("id") String id) throws Exception{
 		DeleteArticleDtoRes result = articleService.deleteById(id);
 		return new ResponseEntity<DeleteArticleDtoRes>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("last-two")
+	public ResponseEntity<GetLastTwoArticleDtoRes> getLastTwo() throws Exception{
+		GetLastTwoArticleDtoRes article = articleService.getLastTwo();
+		return new ResponseEntity<GetLastTwoArticleDtoRes>(article, HttpStatus.OK);		
 	}
 }
