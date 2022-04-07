effe1d9194fd8184c0d2cdc0b652cf65fe860b7b
Iqbal Aditama <iqbaladitama91@gmail.com>
2022-04-07 21:46:58 +0700

    add getByThreadId in PollingHeaderDao

--- community/src/main/java/com/lawencon/community/dao/PollingHeaderDao.java
+++ community/src/main/java/com/lawencon/community/dao/PollingHeaderDao.java
@@ -4,8 +4,32 @@ import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
 import com.lawencon.community.model.PollingHeader;
+import com.lawencon.community.model.Thread;
 
 @Repository
 public class PollingHeaderDao extends AbstractJpaDao<PollingHeader>{
 
+	public PollingHeader getByThreadId(String threadId) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("SELECT ph.id,ph.thread_id,ph.title ");
+		builder.append("FROM polling_header ph ");
+		builder.append("WHERE ph.thread_id = :id");
+		Object result = createNativeQuery(builder.toString())
+				.setParameter("id", threadId)
+				.getSingleResult();
+		Object[] obj = (Object[]) result;
+		
+		PollingHeader pollingHeader = new PollingHeader();
+		if(obj != null) {
+		pollingHeader.setId(obj[0].toString());
+		
+		Thread thread = new Thread();
+		thread.setId(obj[1].toString());
+		pollingHeader.setThread(thread);
+		pollingHeader.setTitle(obj[2].toString());
+		}
+		
+		return pollingHeader;
+	}
+	
 }
