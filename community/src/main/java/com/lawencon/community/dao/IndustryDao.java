15a47b90092c972cbb7db8daf5056f3a43ef824d
ivanigk13 <ivangrashinton@gmail.com>
2022-04-08 11:28:05 +0700

    adding get by bk for activity type, category, city, industry, position, thread type, transaction status

--- community/src/main/java/com/lawencon/community/dao/IndustryDao.java
+++ community/src/main/java/com/lawencon/community/dao/IndustryDao.java
@@ -7,5 +7,18 @@ import com.lawencon.community.model.Industry;
 
 @Repository
 public class IndustryDao extends AbstractJpaDao<Industry>{
-
+	
+	public Integer isIndustryCodeExist(String code) {
+		String sql = "SELECT COUNT(id) FROM industry where industry_code = :code";
+		Object result = createNativeQuery(sql).setParameter("code", code).getSingleResult();
+		Integer flag = Integer.valueOf(result.toString());
+		return flag;
+	}
+	
+	public Integer isIndustryNameExist(String name) {
+		String sql = "SELECT COUNT(id) FROM industry where industry_name = :name";
+		Object result = createNativeQuery(sql).setParameter("name", name).getSingleResult();
+		Integer flag = Integer.valueOf(result.toString());
+		return flag;
+	}
 }
