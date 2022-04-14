99ccb438bc1a9cb0d3dbad44b1bc29e155985375
ivanigk13 <ivangrashinton@gmail.com>
2022-04-14 11:16:16 +0700

    add new method for generate number

--- community/src/main/java/com/lawencon/community/service/BaseCommunityService.java
+++ community/src/main/java/com/lawencon/community/service/BaseCommunityService.java
@@ -33,4 +33,15 @@ public class BaseCommunityService extends BaseService {
 		}
 		return sb.toString();
 	}
+	
+	public String generateVerificationCode(int input) {
+		String codeString = "1234567890";
+		StringBuilder sb = new StringBuilder(input);
+		
+		for (int i = 0; i < input; i++) {
+			int index = (int) (codeString.length() * Math.random());
+			sb.append(codeString.charAt(index));
+		}
+		return sb.toString();
+	}
 }
